02b05dca767a18e5e8b5ae64d9dfeac46c865e5a
nayeli <nayelimoralessy@hotmail.com>
2020-08-10 06:39:25 -0700

    SPLASH SCREEN and BT

--- app/src/main/java/com/example/heartwave/MainActivity.java
+++ app/src/main/java/com/example/heartwave/MainActivity.java
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Set;
 
@@ -19,33 +20,31 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_ENABLE_BT = 0;
     private static final int REQUEST_DISCOVER_BT = 1;
     private BluetoothAdapter ba;
-    private Set<BluetoothAdapter> pairedDevice;
-    private Set<BluetoothDevice> pairedDevices;
-    ImageView mBlue, mNoblue;
-    ImageButton mBtn;
+    FloatingActionButton mactBT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBtn = findViewById(R.id.buttonbt);
-        mBlue = findViewById(R.id.bt);
-        mNoblue = findViewById(R.id.nobt);
+        //mBtn = findViewById(R.id.buttonbt);
+        mactBT = findViewById(R.id.actBT);
+        //mBlue = findViewById(R.id.bt);
+        //mNoblue = findViewById(R.id.nobt);
         ba = BluetoothAdapter.getDefaultAdapter();
 
        // pairedDevices = ba.getBondedDevices();
 
         if (ba.isEnabled()) {
             //mBlue.setImageResource();
-            mBtn.setImageResource(R.drawable.blue);
+            mactBT.setImageResource(R.drawable.blue);
         } else {
-            mBtn.setImageResource(R.drawable.noblue);
+            mactBT.setImageResource(R.drawable.noblue);
             //mNoblue.setImageResource(R.drawable.noblue);
         }
 
         // On Btn click
-        mBtn.setOnClickListener(new View.OnClickListener() {
+        mactBT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!ba.isEnabled()) {
@@ -64,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
             case REQUEST_ENABLE_BT:
                 if (resultCode == RESULT_OK) {
                     // Bluetooth is on
-                    mBtn.setImageResource(R.drawable.blue);
+                    mactBT.setImageResource(R.drawable.blue);
                     showToast("Bluetooth is on");
                 } else {
                     // User denied to turn on bluetooth
